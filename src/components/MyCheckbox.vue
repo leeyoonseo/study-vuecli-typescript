2601918c1cbe50d7b5554637c6d781877de45d43
leeyoonseo <okayoon.lee@gmail.com>
2022-02-28 22:04:18 +0900

    update: 체크박스 컴포넌트 (emit, event)

---
+++ src/components/MyCheckbox.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="checkbox" :checked="checked" @change="handleChange" />
+</template>
+<script lang="ts">
+export default {
+  name: "MyCheckbox",
+};
+</script>
+<script lang="ts" setup>
+import { defineProps, defineEmits, withDefaults } from "vue";
+
+interface IProps {
+  checked?: boolean;
+}
+
+const emit = defineEmits(["change"]);
+
+withDefaults(defineProps<IProps>(), {
+  checked: false,
+});
+
+const handleChange = (e: Event) => {
+  emit("change", (e.target as HTMLInputElement).checked);
+};
+</script>
---
+++ src/views/Agree.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Agree</div>
+  <p>
+    <MyCheckbox id="myCheckbox" v-model="checked" @change="handleChange" />
+    <label for="myCheckbox">{{ label }}</label>
+  </p>
+</template>
+<script lang="ts">
+export default {
+  name: "PageAgree",
+};
+</script>
+<script lang="ts" setup>
+import { ref } from "vue";
+import MyCheckbox from "@/components/MyCheckbox.vue";
+
+const checked = ref(false);
+const label = ref("동의하지않습니다.");
+
+const handleChange = (isChecked: boolean) => {
+  checked.value = isChecked;
+  label.value = isChecked ? "동의합니다." : "동의하지않습니다.";
+};
+</script>
