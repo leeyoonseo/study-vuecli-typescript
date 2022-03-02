507dd1f4cebc5e9014fc6d053a95d6c7d9c4d79f
leeyoonseo <okayoon.lee@gmail.com>
2022-03-02 21:48:26 +0900

    update: pinia Subscribing to actions 추가

--- src/store/counter.ts
+++ src/store/counter.ts
@@ -1,9 +1,14 @@
 import { defineStore } from "pinia";
 
+interface State {
+  counter: number;
+}
+
 export const storeCounter = defineStore("counter", {
-  state: () => ({
-    counter: 0,
-  }),
+  state: () =>
+    ({
+      counter: 0,
+    } as State),
   getters: {},
   actions: {
     increment() {
--- src/store/index.ts
+++ src/store/index.ts
@@ -9,6 +9,38 @@ type ObjType = {
 // https://velog.io/@yyeonjju/TypeScript-Index-Signature-string-key%EB%A1%9C-%EA%B0%9D%EC%B2%B4%EC%97%90-%EC%A0%91%EA%B7%BC%ED%95%98%EA%B8%B0
 
 pinia.use(({ options, store }) => {
+  // * Subscribing to actions
+  if (store) {
+    store.$onAction(({ name, args, after, onError }) => {
+      console.log(
+        `%c 🍍 Event Name: ${name}`,
+        "background: #222; color: #bada55"
+      );
+      const startTime = Date.now();
+      console.log(
+        `%c 🍍 Start: '${name}' with params [${args.join(", ")}].`,
+        "background: #222; color: #bada55"
+      );
+      after((result) => {
+        console.log(
+          `%c 🍍 Finished: '${name}' after ${
+            Date.now() - startTime
+          }ms.\nResult: ${result}.`,
+          "background: #222; color: #bada55"
+        );
+      });
+      onError((error) => {
+        console.log(
+          `%c 🍍 Failed "${name}" after ${
+            Date.now() - startTime
+          }ms.\nError: ${error}.`,
+          "background: red; color: white"
+        );
+      });
+    });
+  }
+
+  // * Debounce
   if (options.debounce) {
     return Object.keys(options.debounce).reduce(
       (debouncedActions, action: string) => {
